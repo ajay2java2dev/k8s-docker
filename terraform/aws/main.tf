697b259ebb5307b4400b31ba83df8f62791442bc
Kalathil Ajay Menon <ajay.java.dev@gmail.com>
2023-04-04 21:09:24 -0500

    start terra

---
+++ terraform/aws/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "tcb-app-dev-jr1"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
